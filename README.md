caa030a92d96828949025ed46cbfdab51f36ca3d
Jamie J. Ball <waytoosalty@gmail.com>
2018-07-24 19:54:36 +1000

    xd

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Australian Fake ID Generator
+Generates random details based off of australian identity cards and popular locations and names.
+
+## How to run
+YOU MUST HAVE PYTHON 3
+1. Open CMD
+2. Goto the directory with the ccgen2.py file
+3. Type in your CMD `py ccgen2.py`
--- ccgen2.py
+++ ccgen2.py
@@ -1,23 +1,24 @@
 import random
+import string
 print("Welcome to Random Australian ID Generator")
 print(" ")
 print("<----- Basic Details ----->")
 
-fname = ['Sophia', 'Jackson', 'Emma', 'Aiden', 'Lucas', 'Liam', 'Ava', 'Mia', 'Noah', 'Isabella', 'Ethan', 'Riley', 'Mason', 'Aria', 'Caden', 'Zoe', 'Oliver', 'Charlotte', 'Elijah', 'Lily', 'Jacob', ]
+fname = ['Sophia', 'Jackson', 'Emma', 'Aiden', 'Lucas', 'Liam', 'Ava', 'Mia', 'Noah', 'Isabella', 'Ethan', 'Riley', 'Mason', 'Aria', 'Caden', 'Zoe', 'Oliver', 'Charlotte', 'Elijah', 'Lily', 'Jacob', 'John', 'Lachlan', 'Georgia' ]
 lname = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis', 'Garcia', 'Rodriguez', 'Wilson', 'White', 'Martin', 'Anderson', 'Thompson', 'Nguyen', 'Turner', 'Walker', ]
 secure_random = random.SystemRandom()
 print("Name:", secure_random.choice(fname), secure_random.choice(lname))
 
-stname = ['Alder', 'Browning', 'Guthrie', 'High', 'Druid', 'Main', 'Lockwood', 'Queen', 'MacKenzie', 'King', 'Mistletoe', 'First', 'Second', 'Third', 'Angas', 'Fosters', 'Regency', 'Newbon', 'Percy', 'Bruce', 'Pirie', 'Currie', 'Gilbert', 'Waymouth', 'Flinders', 'Turner', 'Melrose', 'Silver', 'Turnbull', 'Orana', 'Briens', 'Wright', 'Davies', ]
+stname = ['Alder', 'Browning', 'Guthrie', 'High', 'Druid', 'Folland', 'Main', 'Lockwood', 'Queen', 'MacKenzie', 'King', 'Mistletoe', 'First', 'Second', 'Third', 'Angas', 'Fosters', 'Regency', 'Newbon', 'Percy', 'Bruce', 'Pirie', 'Currie', 'Gilbert', 'Waymouth', 'Flinders', 'Turner', 'Melrose', 'Silver', 'Turnbull', 'Orana', 'Briens', 'Wright', 'Davies', 'Church', 'Chapel', 'Hill', 'Robins']
 sttype = ['St', 'Rd', 'Ct', 'Ave', 'Ln', 'Cres']
 secure_random = random.SystemRandom()
 print("Address:", random.randint(1,75), secure_random.choice(stname), secure_random.choice(sttype),)
 
-city = ['Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Hobart, TAS', 'Brisbane, QLD', 'Surfers Paradise, QLD', 'Bendigo, VIC', 'Ballarat, VIC', 'Newcastle, NSW', 'Cairns, QLD', 'Townsville, QLD', 'Wollongong, NSW', 'Alice Springs, NT', 'Canberra, ACT', 'Darwin, NT', 'Toowoomba, QLD', 'Riverton, SA']
+city = ['Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Hobart, TAS', 'Brisbane, QLD', 'Surfers Paradise, QLD', 'Bendigo, VIC', 'Ballarat, VIC', 'Newcastle, NSW', 'Cairns, QLD', 'Townsville, QLD', 'Wollongong, NSW', 'Alice Springs, NT', 'Canberra, ACT', 'Darwin, NT', 'Toowoomba, QLD', 'Riverton, SA' 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Brisbane, QLD', 'Brisbane, QLD', 'Brisbane, QLD',]
 secure_random = random.SystemRandom()
 print("City:", secure_random.choice(city))
 
-print("Age:", random.randint(18,99))
+print("Age:", random.randint(18,68))
 
 print(" ")
 print("<----- Credit/Debit Card ----->")
@@ -25,13 +26,12 @@ ccn = ['4448-8637-3225-7970', '4755-8490-8118-3724', '4019-1219-4794-6914', '415
 secure_random = random.SystemRandom()
 print("Credit Card Number:", secure_random.choice(ccn))
 
-exp = ['08/24', '07/23', '04/21', '08/19']
+expm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+expy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ]
 secure_random = random.SystemRandom()
-print("Expiry:", secure_random.choice(exp))
+print("Expiry:", secure_random.choice(expm), secure_random.choice(expy))
 
-ccv = ['767', '816', '612', '912', '817', '827', '753', '621', '721', '562', '231']
-secure_random = random.SystemRandom()
-print("CCV:", secure_random.choice(ccv))
+print("CCV:", random.randint(211,982))
 
 typ = ['Visa']
 secure_random = random.SystemRandom()
@@ -44,8 +44,37 @@ print("Bank:", secure_random.choice(bank))
 print(" ")
 print("<----- Physical Infomation ----->")
 
-bt = ['O-', 'O+', 'B-', 'B+', 'A-', 'A+', 'AB-', 'AB+']
+bt = ['O-', 'O+', 'B-', 'B+', 'A-', 'A+', 'AB+', 'AB-', 'O-', 'O+', 'B-', 'B+', 'A-', 'A+', 'AB+']
 secure_random = random.SystemRandom()
 print("Blood Type:", secure_random.choice(bt))
 
-print("Weight:", random.randint(32,90),"kg")
+print("Weight:", random.randint(45,90),"kg")
+
+print("Height:", random.randint(140,168),"cm")
+
+print(" ")
+print("<----- ID Cards ----->")
+
+print("<-- Medicare -->")
+print("Medicare Number:", random.randint(3401,5923), random.randint(19223,72813), random.randint(1,4))
+print("Medicare IRN:", random.randint(1,4))
+medexpm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+medexpy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ]
+secure_random = random.SystemRandom()
+print("Medicare Expiry:", secure_random.choice(medexpm), secure_random.choice(medexpy))
+
+print(" ")
+print("<-- Passport -->")
+print("Passport Number:", random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), random.randint(1927492,6927102))
+pasexpm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+pasexpy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ]
+secure_random = random.SystemRandom()
+print("Passport Expiry:",random.randint(1,28), secure_random.choice(pasexpm), secure_random.choice(pasexpy))
+pasissm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+pasissy = ['2014', '2015', '2016', '2017', '2018' ]
+secure_random = random.SystemRandom()
+print("Passport Date of Issue:",random.randint(1,28), secure_random.choice(pasissm), secure_random.choice(pasissy))
+paspob = ['North Adelaide', 'Modbury', 'Melbourne', 'Sydney', 'Brisbane', 'Perth', 'Darwin', 'Alice Springs', 'Gold Coast', 'Hobart', 'Cairns', 'Melbourne', 'Sydney', 'Melbourne', 'Sydney', 'Melbourne', 'Sydney',]
+secure_random = random.SystemRandom()
+print("Passport Place of Birth:", secure_random.choice(paspob))
+print(" ")
