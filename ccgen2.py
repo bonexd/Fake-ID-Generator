bc1ac28b2041081e2d60ea9798cfb9f5f38aa09e
cth103 <cthdev@email.com>
2019-10-23 11:16:12 +0100

    fix unneccesarry , at end of array

--- ccgen2.py
+++ ccgen2.py
@@ -1,23 +1,23 @@
-# <-- Imports -->
+# Imports
 import random
 import string
-# <-- All Possible Outcomes -->
+# All Possible Outcomes
 # first name
 fname = ['Sophia', 'Jackson', 'Emma', 'Aiden', 'Lucas', 'Liam', 'Ava', 'Mia', 'Noah', 'Isabella', 'Ethan', 'Riley', 'Mason', 'Aria', 'Caden', 'Zoe', 'Oliver', 'Charlotte', 'Elijah', 'Lily', 'Jacob', 'John', 'Lachlan', 'Georgia' ]
 # last name
-lname = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis', 'Garcia', 'Rodriguez', 'Wilson', 'White', 'Martin', 'Anderson', 'Thompson', 'Nguyen', 'Turner', 'Walker', ]
+lname = ['Smith', 'Johnson', 'Williams', 'Brown', 'Jones', 'Miller', 'Davis', 'Garcia', 'Rodriguez', 'Wilson', 'White', 'Martin', 'Anderson', 'Thompson', 'Nguyen', 'Turner', 'Walker' ]
 # street name
 stname = ['Alder', 'Browning', 'Guthrie', 'High', 'Druid', 'Folland', 'Main', 'Lockwood', 'Queen', 'MacKenzie', 'King', 'Mistletoe', 'First', 'Second', 'Third', 'Angas', 'Fosters', 'Regency', 'Newbon', 'Percy', 'Bruce', 'Pirie', 'Currie', 'Gilbert', 'Waymouth', 'Flinders', 'Turner', 'Melrose', 'Silver', 'Turnbull', 'Orana', 'Briens', 'Wright', 'Davies', 'Church', 'Chapel', 'Hill', 'Robins']
 # street type
 sttype = ['St', 'Rd', 'Ct', 'Ave', 'Ln', 'Cres']
 # city of residence
-city = ['Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Hobart, TAS', 'Brisbane, QLD', 'Surfers Paradise, QLD', 'Bendigo, VIC', 'Ballarat, VIC', 'Newcastle, NSW', 'Cairns, QLD', 'Townsville, QLD', 'Wollongong, NSW', 'Alice Springs, NT', 'Canberra, ACT', 'Darwin, NT', 'Toowoomba, QLD', 'Riverton, SA' 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Brisbane, QLD', 'Brisbane, QLD', 'Brisbane, QLD',]
+city = ['Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Hobart, TAS', 'Brisbane, QLD', 'Surfers Paradise, QLD', 'Bendigo, VIC', 'Ballarat, VIC', 'Newcastle, NSW', 'Cairns, QLD', 'Townsville, QLD', 'Wollongong, NSW', 'Alice Springs, NT', 'Canberra, ACT', 'Darwin, NT', 'Toowoomba, QLD', 'Riverton, SA' 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Adelaide, SA', 'Melbourne, VIC', 'Sydney, NSW', 'Perth, WA', 'Brisbane, QLD', 'Brisbane, QLD', 'Brisbane, QLD']
 # credit card number
 ccn = ['4448-8637-3225-7970', '4755-8490-8118-3724', '4019-1219-4794-6914', '4157-1447-4760-4063', '4188-9273-0003-7178']
 # credit card expiry month
 expm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 # credit card expiry year
-expy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ]
+expy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029']
 # credit card provider
 typ = ['Visa']
 # bank
@@ -27,19 +27,19 @@ bt = ['O-', 'O+', 'B-', 'B+', 'A-', 'A+', 'AB+', 'AB-', 'O-', 'O+', 'B-', 'B+', 
 # medicare expiry month
 medexpm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 # medicare expiry year
-medexpy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ]
+medexpy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029']
 # passport expiry month
 pasexpm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 # passport expiry year
-pasexpy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', ]
+pasexpy = ['2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029']
 # passport issuing month
 pasissm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 # passport issuing year
 pasissy = ['2014', '2015', '2016', '2017', '2018' ]
 # passport place of birth
-paspob = ['North Adelaide', 'Modbury', 'Melbourne', 'Sydney', 'Brisbane', 'Perth', 'Darwin', 'Alice Springs', 'Gold Coast', 'Hobart', 'Cairns', 'Melbourne', 'Sydney', 'Melbourne', 'Sydney', 'Melbourne', 'Sydney',]
+paspob = ['North Adelaide', 'Modbury', 'Melbourne', 'Sydney', 'Brisbane', 'Perth', 'Darwin', 'Alice Springs', 'Gold Coast', 'Hobart', 'Cairns', 'Melbourne', 'Sydney', 'Melbourne', 'Sydney', 'Melbourne', 'Sydney']
 
-# <-- The Script -->
+# The Script
 print("Welcome to Random Australian ID Generator")
 print(" ")
 print("<----- Basic Details ----->")
@@ -65,8 +65,8 @@ print(" ")
 print("<----- Physical Infomation ----->")
 secure_random = random.SystemRandom()
 print("Blood Type:", secure_random.choice(bt))
-print("Weight:", random.randint(45,90),"kg")
-print("Height:", random.randint(140,168),"cm")
+print("Weight:", random.randint(45,90), "kg")
+print("Height:", random.randint(140,168), "cm")
 print(" ")
 print("<----- ID Cards ----->")
 print("<-- Medicare -->")
